b80f746f04916101157c61d4639c650a5e56aec0
dahyunJung <deh2246@naver.com>
2021-10-27 03:02:35 +0900

    시간대 변경

--- src/main/java/com/kim/KimApplication.java
+++ src/main/java/com/kim/KimApplication.java
@@ -2,6 +2,7 @@ package com.kim;
 
 import com.kim.test.FileProperties;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.tomcat.jni.Time;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
@@ -10,12 +11,20 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableConfigurationProperties({ FileProperties.class })
 @MapperScan(value = {"com.kim.test.mapper"})
 public class KimApplication {
+
+    @PostConstruct
+    void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(KimApplication.class, args);
     }
